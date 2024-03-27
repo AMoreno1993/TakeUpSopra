4447d567bed3ef80fcce6fda4fb3b065c3f37fcb
MONTANES Alejandro <alejandro.moreno3@ext.soprasteria.com>
2024-03-27 09:31:25 +0100

    Feat(components):Training components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import dataJson from 'src/app/products.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  ngOnInit() {
+    console.log;
+  }
+}

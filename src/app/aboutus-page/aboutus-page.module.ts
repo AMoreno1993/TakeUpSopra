be1bfb699d54429def235b711a9f47fd3e5fb7ef
AMoreno1993 <alejandro.moreno3@ext.soprasteria.com>
2024-04-10 12:38:42 +0200

    Feat(modules):modules aboutUs created with components

--- src/app/aboutus-page/aboutus-page.module.ts
+++ src/app/aboutus-page/aboutus-page.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { AboutusRoutingModule } from './aboutus-routing.module';
+import { MoreInfoComponent } from './components/more-info/more-info.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
 
 @NgModule({
-  declarations: [AboutUsComponent],
+  declarations: [
+    AboutUsComponent,
+    SubscribeComponent,
+    MoreInfoComponent,
+    AboutUsComponent,
+  ],
   imports: [CommonModule, AboutusRoutingModule],
 })
 export class AboutusPageModule {}
